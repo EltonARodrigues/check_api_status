7bfb43ca16e1360e20daf963bacb3b28041c6a4a
Elton de Andrade Rodrigues <eltonrodrigues61@gmail.com>
2022-04-15 16:52:51 -0300

    logic to configure body, http method and status result

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,31 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
+use crate::Value::Null;
 use clap::Arg;
 use clap::Command;
 use notify_rust::Notification;
-use reqwest::StatusCode;
+use reqwest::Method;
+use reqwest::Response;
+use reqwest::{Client, StatusCode, Url};
 use serde_json::Error;
 use serde_json::Value;
-use std::fs;
-use std::{env, fs::File, io::prelude::*, thread, time::Duration};
+use std::{fs, thread, time::Duration};
 
-const CONFIG_FILE: &str = "config.json";
+#[derive(PartialEq, Clone, Serialize, Deserialize, Debug)]
+enum ConfigMethod {
+    GET,
+    POST,
+    // DELETE,
+    // PUT,
+    // PATH,
+}
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
 struct Request {
     url: String,
     headers: Value,
+    method: ConfigMethod,
     body: Value,
 }
 
@@ -26,16 +36,11 @@ struct Request {
 struct Config {
     name: String,
     request: Request,
+    expected_status: u16,
     time_loop: u64,
     gnome_notify: bool,
     notify_type: String,
     show: bool,
-}
-
-#[tokio::main]
-async fn api_config(url: &str) -> Result<StatusCode, Box<dyn std::error::Error>> {
-    let resp = reqwest::get(url).await?;
-    Ok(resp.status())
 }
 
 fn notify(name: &str, icon_type: &str, message: &str) -> Result<(), Box<dyn std::error::Error>> {
@@ -47,11 +52,40 @@ fn notify(name: &str, icon_type: &str, message: &str) -> Result<(), Box<dyn std:
     Ok(())
 }
 
-fn verify_api(name: &str, url: &str, notify_type: &str) {
-    let response = api_config(url);
+#[tokio::main]
+async fn api_config(request: &Request) -> Result<StatusCode, Box<dyn std::error::Error>> {
+    let client = Client::builder().build()?;
 
-    let f = match response {
-        Ok(file) => file,
+    // Missing all methods
+    let method: &'static [u8] = if request.method == ConfigMethod::POST {
+        b"POST"
+    } else {
+        b"GET"
+    };
+
+    // TODO
+    let resp: Response;
+    if request.body != Null {
+        resp = client
+            .request(Method::from_bytes(method)?, Url::parse(&request.url)?)
+            .json(&request.body)
+            .send()
+            .await?;
+    } else {
+        resp = client
+            .request(Method::from_bytes(method)?, Url::parse(&request.url)?)
+            .send()
+            .await?;
+    };
+
+    Ok(resp.status())
+}
+
+fn verify_api(name: &str, request: &Request, expected_status: &u16, notify_type: &str) {
+    let response = api_config(request);
+
+    let status = match response {
+        Ok(status_code) => status_code.as_u16(),
         Err(e) => {
             println!("{0}: OK -> {1}", name, e);
             notify(
@@ -63,13 +97,14 @@ fn verify_api(name: &str, url: &str, notify_type: &str) {
             return ();
         }
     };
-    if f == 200 {
-        println!("{0}: OK -> {1}", name, f);
+
+    if &status == expected_status {
+        println!("{0}: OK -> {1}", name, status);
         if notify_type != "ERROR" {
             notify(name, "dialog-information", "Tudo normal segue o jogo!").unwrap();
         }
     } else {
-        println!("{0}: ERROR -> {1}", name, f);
+        println!("{0}: ERROR -> {1}", name, status);
         notify(
             name,
             "dialog-error",
@@ -83,7 +118,12 @@ fn start_monitor(config: Config) -> thread::JoinHandle<()> {
     return thread::spawn(move || loop {
         thread::sleep(Duration::from_millis(config.time_loop));
         println!("Verifing {0} ...", config.name);
-        verify_api(&config.name, &config.request.url, &config.notify_type);
+        verify_api(
+            &config.name,
+            &config.request,
+            &config.expected_status,
+            &config.notify_type,
+        );
     });
 }
 
@@ -119,15 +159,15 @@ fn main() -> std::io::Result<()> {
         .unwrap_or_else(|| panic!("File not set"));
 
     let config_object = load_config(config_path);
-
+    println!("{:?}", config_object);
     let mut monitor_threads = Vec::new();
 
     match config_object {
         Ok(configs) => {
             for config in configs.iter() {
                 println!("Start thread to monitor endpoint: {0}", config.name);
-                let aa = start_monitor(config.clone());
-                monitor_threads.push(aa);
+                let api_thread = start_monitor(config.clone());
+                monitor_threads.push(api_thread);
             }
 
             for handle in monitor_threads {
@@ -137,33 +177,6 @@ fn main() -> std::io::Result<()> {
         }
         Err(e) => println!("error parsing: {:?}", e),
     }
-
-    // println!("{:?}", resp);
-    // println!("In file {}", filename);
-    // let mut file = File::open(CONFIG_FILE)?;
-    // let mut contents = String::new();
-
-    // file.read_to_string(&mut contents)?;
-
-    // println!("Load config file");
-    // let resp = load_config(contents);
-
-    // let mut threads = Vec::new();
-    // match resp {
-    //     Ok(configs) => {
-    //         for config in configs.iter() {
-    //             println!("Start thread to monitor endpoint: {0}", config.name);
-    //             let aa = start_monitor(config);
-    //             threads.push(aa);
-    //         }
-
-    //         for handle in threads {
-    //             thread::sleep(Duration::from_millis(3000));
-    //             handle.join().unwrap()
-    //         }
-    //     }
-    //     Err(e) => println!("error parsing: {:?}", e),
-    // }
 
     Ok(())
 }
