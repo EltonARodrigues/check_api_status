8b0db3a970dda778305556f5fc3256a685a8b768
Elton A Rodrigues <eltonrodrigues61@gmail.com>
2022-06-26 12:13:55 -0300

    Remove option to send a email with status

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,2 @@
 pub mod notify;
 pub mod yarn;
-pub mod email;
--- src/utils/notify.rs
+++ src/utils/notify.rs
@@ -1,14 +1,10 @@
 use notify_rust::Notification;
 
-use crate::utils::email::send_email;
-
 pub fn send_notify(
     name: &str,
     icon_type: &str,
     message: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    send_email(message.to_string());
-
     Notification::new()
         .summary(name)
         .body(message)
